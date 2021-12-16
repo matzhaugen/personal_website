53b3bb2ff6178f0120ad8332e58bb5a818ccfdee
Matz Haugen <mhaugen@vise.com>
2021-12-16 12:33:51 +0100

    test period layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,9 +4,8 @@
   <a href="/research">Research</a>
   <a href="/links">Links</a>
   <a href="/software">Software</a> 
-  <!-- <a href="/blog">Blog</a>  -->
-  <a href="/MatzHaugenResume.pdf" download="MatzHaugenResume">CV
-	</a>
+  <a href="/MatzHaugenResume.pdf" download="MatzHaugenResume">CV</a>
+  <a href="/blog">.</a> 
 </nav>
 <slot></slot>
 </body>
