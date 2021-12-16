79103b5d360ecb3fd0ac80772fd5574881c27d8c
Matz Haugen <mhaugen@vise.com>
2021-12-16 13:07:58 +0100

    remove ssr

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,7 @@ import adapter from '@sveltejs/adapter-netlify';
 export default {
 	kit: {
 		adapter: adapter(),
-		prerender: {
-	      crawl: true,
-	      enabled: true,
-	      onError: 'continue',
-	      entries: ['*'],
-	    },
 		target: '#svelte',
-		ssr: false
 	}
 
 };
