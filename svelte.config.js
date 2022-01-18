fbc950c2b8d6101abe8734d5b687c149f10f3084
Matz Haugen <mhaugen@vise.com>
2022-01-18 18:32:09 +0100

    remove repository

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
 import adapter from '@sveltejs/adapter-netlify';
 import path from 'path'
 import { mdsvex } from 'mdsvex'
-import remarkGfm from 'remark-gfm'
-import github from 'remark-github'
 
 export default {
 	extensions: ['.svelte', '.svx', '.md'],
 	preprocess: mdsvex({ 
-		remarkPlugins: [remarkGfm, github],
 		extensions: ['.svx', '.md'] }),
 
 		
