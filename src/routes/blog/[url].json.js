3518ee04400b5149662cbf7230073768504fcfb0
Matz Haugen <mhaugen@vise.com>
2022-01-19 12:01:18 +0100

    update

--- src/routes/blog/[url].json.js
+++ src/routes/blog/[url].json.js
@@ -4,5 +4,6 @@ export function get({ params }) {
     const {url} = params;
     const key = `./posts/${url}.md`
     let body = JSON.stringify({metadata: imports[key].metadata, ...imports[key].default.render()})
+
     return {body}
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
-import path from 'path'
 import { mdsvex } from 'mdsvex'
+import remarkFootnotes from 'remark-footnotes'
+
 
 export default {
 	extensions: ['.svelte', '.svx', '.md'],
 	preprocess: mdsvex({ 
+		remarkPlugins: [remarkFootnotes],
 		extensions: ['.svx', '.md'] }),
 
 		
