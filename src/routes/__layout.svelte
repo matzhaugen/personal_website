c6d790ffdac63aa86523a5d943576580ebc0e8d4
Matz Haugen <mhaugen@vise.com>
2022-11-24 22:45:57 +0100

    add fan wu

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,7 +12,8 @@
   <a href="/software">Software</a> 
   <a href="/MatzHaugenResume.pdf" download="MatzHaugenResume">CV</a>
   <a href="/blog">Blog</a> 
-  <a href="/satellites">Satellites</a> 
+  <a href="/satellites">Satellites</a>
+  <img id="sun" src="/sun.svg" alt="a" width="3%" height="2%" style="vertical-align:middle;margin:5px 5px 6px 2px">
 </nav>
 <slot></slot>
 </body>
