d8b2cf030f7b16f5f511b8a9cd37b8a2f0d1ea89
Matz Haugen <mhaugen@vise.com>
2023-01-20 10:54:23 +0100

    redirect

---
+++ src/routes/blog/[slug]/+page.js
@@ -0,0 +1,8 @@
+import { error } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
+export function load({ params }) {
+  if (params.slug = "excess-mort-en") {
+    throw redirect(307, 'https://electromagneticlife.substack.com/p/excess-mortality-compared-to-vaccination');
+    }
+  throw error(404, 'Not found');
+}
