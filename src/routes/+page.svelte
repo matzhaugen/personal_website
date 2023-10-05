3d94dd44eaa8898b2616dbb5f6c2d2542f915980
Matz Haugen <matzhaugen@Matzs-MacBook-Pro.local>
2023-10-05 14:58:20 +0200

    update experience

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <h1> About me </h1>
 
 <p>
-I have developed algorithms for the extraction of signals embedded in potentially incomplete multivariate time series and predicting extreme climate events through non-parametric statistical techniques. Currently, I'm working for <a href="https://vise.com/">Vise Technologies, Inc.</a> building optimal portfolios using convex optimization techniques and python.
+I have developed algorithms for the extraction of signals embedded in potentially incomplete multivariate time series and predicting extreme climate events through non-parametric statistical techniques. Currently, I'm working for <a href="https://vise.com/">Vise Technologies, Inc.</a> building optimal portfolios using convex optimization techniques and python. 
 </p>
 <h3>Key words: </h3> <i>Principal Component Analysis, Linear Dimensionality Reduction, Quantile Regression, Optimization, Covariance/Graph Estimation, Machine Learning, Hypothesis Testing.</i>
 
@@ -13,3 +13,11 @@ I have developed algorithms for the extraction of signals embedded in potentiall
 	<li> B.Sc, <i>Physics</i>, <a href="https://www.mcgill.ca/" > McGill University </a> , 2008.</li>
 </ul>
 
+<h2> Experience </h2>
+<ul>
+	<li>  <i>Senior Quantitative Engineer</i>, <a href="https://www.vise.com/" > Vise Technologies Inc. </a>, 2021-present</li>
+	<li>  <i>Senior Machine Learning Engineer</i>, <a href="https://www.cognite.com/" > Cognite </a>, 2020-2021</li>
+	<li>  <i>Senior Data Scientist</i>,  <a href="https://www.orbitalinsight.com/" > Orbital Insight </a>, 2018-2020</li>
+	<li>  <i>Post-doctoral fellow in Climate Statistics</i>,  <a href="https://www.uchicago.edu/" > University of Chicago </a>, 2017-2018</li>
+	
+</ul>
